1e592fe7bbbc457a396b1a605ebd019987478551
Chenfucius <chen-so-stupid@live.com>
2016-10-14 17:05:02 -0700

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WebcamTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.IBOriginal = new Emgu.CV.UI.ImageBox();
+            this.IBProccessed = new Emgu.CV.UI.ImageBox();
+            this.txtXYRadius = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.IBOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IBProccessed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IBOriginal
+            // 
+            this.IBOriginal.Location = new System.Drawing.Point(0, 2);
+            this.IBOriginal.Name = "IBOriginal";
+            this.IBOriginal.Size = new System.Drawing.Size(640, 480);
+            this.IBOriginal.TabIndex = 2;
+            this.IBOriginal.TabStop = false;
+            // 
+            // IBProccessed
+            // 
+            this.IBProccessed.Location = new System.Drawing.Point(646, 2);
+            this.IBProccessed.Name = "IBProccessed";
+            this.IBProccessed.Size = new System.Drawing.Size(640, 480);
+            this.IBProccessed.TabIndex = 2;
+            this.IBProccessed.TabStop = false;
+            // 
+            // txtXYRadius
+            // 
+            this.txtXYRadius.Location = new System.Drawing.Point(646, 525);
+            this.txtXYRadius.Name = "txtXYRadius";
+            this.txtXYRadius.Size = new System.Drawing.Size(100, 35);
+            this.txtXYRadius.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1287, 715);
+            this.Controls.Add(this.txtXYRadius);
+            this.Controls.Add(this.IBProccessed);
+            this.Controls.Add(this.IBOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.IBOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IBProccessed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox IBOriginal;
+        private Emgu.CV.UI.ImageBox IBProccessed;
+        private System.Windows.Forms.TextBox txtXYRadius;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
+namespace WebcamTracker
+{
+    public partial class Form1 : Form
+    {
+        private Capture capWebcam;
+        private bool blnCapturingInProcess = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            capWebcam = new Capture(CaptureType.Any);
+            try
+            {
+                capWebcam = new Capture();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("unable to read from webcam, error: " + Environment.NewLine + Environment.NewLine +
+                               ex.Message + Environment.NewLine + Environment.NewLine +
+                               "exiting program");
+                Environment.Exit(0);
+                return;
+            }
+            
+            Application.Idle += processFrameAndUpdateGUI;
+            blnCapturingInProcess = true;
+        }
+
+
+        private void processFrameAndUpdateGUI(object sender, EventArgs arg)
+        {
+            Mat imgOriginal;
+
+            imgOriginal = capWebcam.QueryFrame();
+
+            if (imgOriginal == null)
+            {
+                MessageBox.Show("unable to read from webcam" + Environment.NewLine + Environment.NewLine +
+                                "exiting program");
+                Environment.Exit(0);
+                return;
+            }
+
+            Mat imgHSV = new Mat(imgOriginal.Size, DepthType.Cv8U, 3);
+
+            Mat imgThreshLow = new Mat(imgOriginal.Size, DepthType.Cv8U, 1);
+            Mat imgThreshHigh = new Mat(imgOriginal.Size, DepthType.Cv8U, 1);
+
+            Mat imgThresh = new Mat(imgOriginal.Size, DepthType.Cv8U, 1);
+
+            //convert the color configs in imgOriginal and store new image to imgHSV
+            CvInvoke.CvtColor(imgOriginal, imgHSV, ColorConversion.Bgr2Hsv);
+
+            //put data that lies within the threshhold from imgHSV to imgThreshLow
+            CvInvoke.InRange(imgHSV, new ScalarArray(new MCvScalar(0, 155, 155)), new ScalarArray(new MCvScalar(18, 255, 255)), imgThreshLow);
+            CvInvoke.InRange(imgHSV, new ScalarArray(new MCvScalar(165, 155, 155)), new ScalarArray(new MCvScalar(179, 255, 255)), imgThreshHigh);
+
+            //add images together and put into imgThresh
+            CvInvoke.Add(imgThreshLow, imgThreshHigh, imgThresh);
+            
+            //add Gaussian blue to imgThresh to reduce noice
+            CvInvoke.GaussianBlur(imgThresh, imgThresh, new Size(3, 3), 0);
+
+            //size -1 -1 means anchor is at center 
+            Mat structuringElement = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new Size(3, 3), new Point(-1, -1));
+
+            //reduce noise around the object
+            CvInvoke.Dilate(imgThresh, imgThresh, structuringElement, new Point(-1, -1), 1, BorderType.Default, new MCvScalar(0, 0, 0));
+            CvInvoke.Erode(imgThresh, imgThresh, structuringElement, new Point(-1, -1), 1, BorderType.Default, new MCvScalar(0, 0, 0));
+
+            //
+            CircleF[] circles = CvInvoke.HoughCircles(imgThresh, HoughType.Gradient, 2.0, imgThresh.Rows / 4, 100, 50, 10, 400);
+
+            foreach (CircleF circle in circles)
+            {
+                if (txtXYRadius.Text != "")
+                {                         // if we are not on the first line in the text box
+                    txtXYRadius.AppendText(Environment.NewLine);         // then insert a new line char
+                }
+
+                txtXYRadius.AppendText("ball position x = " + circle.Center.X.ToString().PadLeft(4) + ", y = " + circle.Center.Y.ToString().PadLeft(4) + ", radius = " + circle.Radius.ToString("###.000").PadLeft(7));
+                txtXYRadius.ScrollToCaret();             // scroll down in text box so most recent line added (at the bottom) will be shown
+
+                CvInvoke.Circle(imgOriginal, new Point((int)circle.Center.X, (int)circle.Center.Y), (int)circle.Radius, new MCvScalar(0, 0, 255), 2);
+                CvInvoke.Circle(imgOriginal, new Point((int)circle.Center.X, (int)circle.Center.Y), 3, new MCvScalar(0, 255, 0), -1);
+            }
+
+            IBOriginal.Image = imgOriginal;
+            IBProccessed.Image = imgThresh;
+        }
+
+       
+
+        //private void imageBox1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+    }
+}
